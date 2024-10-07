9a345be3ad621078e22d436c2df780a7a5d59c00
chahdj12 <chahed.jouini@esprit.tn>
2024-10-07 16:20:07 +0200

    doctrine use

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -1,18 +1,35 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Client;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client')]
-    public function index(): Response
+    #[Route('/client/add', name: 'add_client')]
+    public function addClient(ManagerRegistry $doctrine): Response
     {
-        return $this->render('client/index.html.twig', [
-            'controller_name' => 'ClientController',
+        $entityManager = $doctrine->getManager();
+        $client = new Client();
+        $client->setNom('chahed');
+        $client->setPrenom('jouini');
+        $client->setAge('12');
+        $client2 = new Client();
+        $client2->setNom('ali');
+        $client2->setPrenom('bensalah');
+        $client2->setAge('12');
+
+        //ajouter loperaton dinsertion
+        $entityManager->persist($client);
+        $entityManager->persist($client2);
+        $entityManager->flush();
+
+        //execute la transaction
+        return $this->render('client/detail.html.twig', [
+            'client' => $client2,
         ]);
     }
 }
